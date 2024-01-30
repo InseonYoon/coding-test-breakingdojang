33e4f4e9692e000dcfd89226f075cec359cdee2b
intotheseon <30719055+InseonYoon@users.noreply.github.com>
2024-01-30 23:48:32 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        int svs = n/10;
-        answer = n*12000 + (k-svs)*2000;
-        
-        System.out.println(n + "인분을 시켜 서비스로 음료수를 " + svs + "개 받아 총 " + (n*12000 + (k-svs)*2000) + "원입니다.");
-        
-        return answer;
+        return answer = n * 12000 + (k - n/10) * 2000;
     }
 }
