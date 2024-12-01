325fcfd7fbe148a8db2dbff1e6c711c9c27756c5
intotheseon <30719055+InseonYoon@users.noreply.github.com>
2024-12-01 19:28:15 +0900

    [level 0] Title: 문자열 섞기, Time: 0.07 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,14 +1,15 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
         
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
         
+        StringBuilder sb = new StringBuilder();
+        
         for(int i=0; i<str1.length(); i++){
-            answer = answer + arr1[i] + arr2[i];
+            sb.append(arr1[i]).append(arr2[i]);
         }
         
-        return answer;
+        return sb.toString();
     }
 }
