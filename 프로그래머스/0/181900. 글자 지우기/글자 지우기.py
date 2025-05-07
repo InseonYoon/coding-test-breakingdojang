93993a8b20d81b266de3734d21efe70dc4018d19
intotheseon <30719055+InseonYoon@users.noreply.github.com>
2025-05-07 18:49:28 +0900

    [level 0] Title: 글자 지우기, Time: 0.08 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -2,8 +2,10 @@ def solution(my_string, indices):
     answer = ''
     indices.sort()
     for i in range(len(my_string)):
-        if i in indices:
-            continue
-        else:
+        # if i in indices:
+        #     continue
+        # else:
+        #     answer += my_string[i]
+        if i not in indices:
             answer += my_string[i]
     return answer
