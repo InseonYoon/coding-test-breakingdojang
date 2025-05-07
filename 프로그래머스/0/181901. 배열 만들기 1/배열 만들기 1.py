da9fb5c7716b8f2886af383776fda80665906559
intotheseon <30719055+InseonYoon@users.noreply.github.com>
2025-05-07 18:33:55 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.12 ms, Memory: 9.61 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,6 +1,7 @@
 def solution(n, k):
-    answer = []
-    maxi = (n//k)
-    for i in range(maxi):
-        answer.append((i+1)*k)
-    return answer
+    # answer = []
+    # maxi = (n//k)
+    # for i in range(maxi):
+    #     answer.append((i+1)*k)
+    # return answer
+    return [i for i in range(k, n+1, k)]
