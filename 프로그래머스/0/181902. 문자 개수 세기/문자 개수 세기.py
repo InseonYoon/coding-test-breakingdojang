9ee00a73916d0048b3846862d1e40b9233c837a2
intotheseon <30719055+InseonYoon@users.noreply.github.com>
2025-05-07 18:27:04 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.32 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,8 +1,14 @@
 def solution(my_string):
     answer = [0 for i in range(52)]
-    test = ''
-    for i in range(len(my_string)):
-        index = ord(my_string[i])-(65 if (ord(my_string[i])<91) else 71) 
-        answer[index] += 1
-        test += my_string[i]+', '+str(ord(my_string[i])-65)+' / '
+    # test = ''
+    # for i in range(len(my_string)):
+    #     index = ord(my_string[i])-(65 if (ord(my_string[i])<91) else 71) 
+    #     answer[index] += 1
+    #     test += my_string[i]+', '+str(ord(my_string[i])-65)+' / '
+    
+    for x in my_string:
+        if x.isupper():
+            answer[ord(x) - 65] += 1
+        else:
+            answer[ord(x) - 71] += 1
     return answer
