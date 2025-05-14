8927989b12a57113e732415295ebee84e831b5f2
intotheseon <30719055+InseonYoon@users.noreply.github.com>
2025-05-15 00:16:07 +0900

    [level 1] Title: 숫자 짝꿍, Time: 96.84 ms, Memory: 26.9 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,22 +1,12 @@
 def solution(X, Y):
     answer = ''
-    ctrX = [0 for i in range(10)]
-    ctrY = [0 for i in range(10)]
-    ctr = [0 for i in range(10)]
 
-    for c in str(X):
-        ctrX[int(c)] += 1
-    for c in str(Y):
-        ctrY[int(c)] += 1
-    for i in range(10):
-        ctr[i] = ctrX[i] if ctrX[i]<= ctrY[i] else ctrY[i]
-        
-    if sum(ctr) == 0:
+    for i in range(9,-1,-1) :
+        answer += (str(i) * min(X.count(str(i)), Y.count(str(i))))
+
+    if answer == '' :
         return '-1'
-    elif sum(ctr[1:10]) == 0:
+    elif len(answer) == answer.count('0'):
         return '0'
-    else:
-        for i in range(9, -1, -1):
-            for j in range(0, ctr[i]):
-                answer += str(i)
+    else :
         return answer
