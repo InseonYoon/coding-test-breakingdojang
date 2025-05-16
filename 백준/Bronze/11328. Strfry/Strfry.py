23974ef7a1e03bae881bed6e6300c32ad9d17ade
intotheseon <30719055+InseonYoon@users.noreply.github.com>
2025-05-16 23:38:24 +0900

    [Bronze II] Title: Strfry, Time: 232 ms, Memory: 36412 KB -BaekjoonHub

---
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+from math import ceil
+
+n = int(input())
+for _ in range(n):
+    a, b = input().split()
+    a = sorted(a)
+    b = sorted(b)
+    if a == b:
+        print('Possible')
+    else:
+        print('Impossible')
