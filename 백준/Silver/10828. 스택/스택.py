f7f146f216cfa2887512b65072e26a1994c8c1ac
intotheseon <30719055+InseonYoon@users.noreply.github.com>
2025-05-22 18:48:22 +0900

    [Silver IV] Title: 스택, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,25 @@
+import sys
+
+orders = sys.stdin.readlines()
+N = int(orders[0].strip())
+stack = []
+
+for i in range(1, N+1):
+    order = orders[i].strip().split()
+
+    if order[0] == 'push':
+        stack.append(order[1])
+    elif order[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif order[0] == 'size':
+        print(len(stack))
+    elif order[0] == 'empty':
+        print(0 if stack else 1)
+    else: # order[0] == 'top'
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
